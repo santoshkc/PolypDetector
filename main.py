bf33f1f0b3f33d7dc7c02e8588a43a8326cdcf0b
Santosh K.C <santoshkc89@gmail.com>
2022-03-30 12:47:54 +0545

    kaggle fix

--- main.py
+++ main.py
@@ -35,6 +35,36 @@ categories = { 'adenomatous': 0, 'hyperplastic': 1 }
 import os
 import time
 
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser(description="Polyp configs")
+    parser.add_argument("--train", action="store_true", help="use for training")
+    parser.add_argument("--infer", action="store_true", help="infer.")
+    
+    parser.add_argument(
+        "--output-dir",
+        help="Save training weights and logs"
+        "If not given, will show output in an OpenCV window.",
+    )
+
+    parser.add_argument(
+        "--confidence-threshold",
+        type=float,
+        default=0.5,
+        help="Minimum score for instance predictions to be shown",
+    )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options using the command-line 'KEY VALUE' pairs",
+        default=[],
+        nargs=argparse.REMAINDER,
+    )
+    return parser
+
+def parse_arguments():
+	pass
+
 if __name__ == "__main__":
 	print(list(categories.keys()))
 	path_prefix = r"../PolypsSet"
@@ -43,7 +73,9 @@ if __name__ == "__main__":
 	def detection_test():
 		#output_folder = f'./result_{time.time()}'
 		#output_folder = './result_1648384510.6090004'
-		output_folder = '/content/drive/MyDrive/detectron_train_2'
+		#output_folder = '/content/drive/MyDrive/detectron_train_2'
+		#output_folder = './detectron_train_pcampus'
+		output_folder = '/kaggle/working/detectron_train_pcampus'
 		polyp_detector = PolypDetector("polyp_train", "polyp_validation", default_output_dir=output_folder)
 
 		should_train = True
@@ -54,6 +86,7 @@ if __name__ == "__main__":
 		should_infer = False
 		if should_infer == True:
 			output_folder = f'./result_{time.time()}'
+			#output_folder = './detectron_train_pcampus'
 			if os.path.exists(output_folder) == False:
 				os.mkdir(output_folder)
 			#polyp_detector.infer("polyp_train", "train_data.txt", path_prefix, output_folder)
